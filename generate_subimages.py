053e53700314084af8ed56e00856ae516ccba43f
bereznik <bernardreznik@gmail.com>
2021-04-24 17:22:43 -0300

    added functionality to ignore alredy seen burnt pixels

--- generate_subimages.py
+++ generate_subimages.py
@@ -4,7 +4,28 @@ from PIL import Image
 import os
 import tifffile
 from ndvi import make_ndvi
+import rasterio
 np.seterr(divide='ignore', invalid='ignore')
+
+def change_names():
+    image_path = '../Binary Masks/Cortes/Images'
+    mask_path = '../Binary Masks/Cortes/Masks'
+
+    for image in os.listdir(image_path):
+        if image.endswith('.xml'):
+            os.remove(os.path.join(image_path,image))
+    for mask in os.listdir(mask_path):
+        if mask.endswith('.xml'):
+            os.remove(os.path.join(mask_path,mask))
+    i = 1
+
+    for image,mask in zip(os.listdir(image_path),os.listdir(mask_path)):
+        os.rename(os.path.join(image_path,image),os.path.join(image_path,'image_' + str(i)))
+        os.rename(os.path.join(mask_path,mask),os.path.join(mask_path,'mask_' + str(i)))
+        i = i+1
+
+
+
 def make_save_subimages(raster_before_path,raster_after_path,im_path_1,im_path_2,out_path,n):
     ''' im_array_path: path to numpy array representing the Geotif Image
         im_path_1: path to binary mask containing class 1 (forest)
@@ -15,8 +36,8 @@ def make_save_subimages(raster_before_path,raster_after_path,im_path_1,im_path_2
     Generates sub images form binary mask randomly but containing for sure target pixels
     '''
     #loading rasters
-    raster_before = np.load(raster_before_path)
-    raster_after = np.load(raster_after_path)
+    raster_before = np.load(raster_before_path).astype(np.int32)
+    raster_after = np.load(raster_after_path).astype(np.int32)
     
     #loading masks
     im1 = plt.imread(im_path_1)
@@ -29,7 +50,7 @@ def make_save_subimages(raster_before_path,raster_after_path,im_path_1,im_path_2
     mask_copy = np.copy(mask) 
 
     ndvi_before,ndvi_after = make_ndvi(raster_before,raster_after)    
-
+    
     size = 256
     for i in range(0,n):
         ar1,ar2 = np.where(mask!=0)
@@ -44,17 +65,19 @@ def make_save_subimages(raster_before_path,raster_after_path,im_path_1,im_path_2
         
         cut_ndvi_after = ndvi_after[c[i]-a:c[i]+size-a,d[i]-b:d[i]+size-b]
         cut_ndvi_before = ndvi_before[c[i]-a:c[i]+size-a,d[i]-b:d[i]+size-b]
-        mean_NIR = (raster_before[c[i]-a:c[i]+size-a,d[i]-b:d[i]+size-b,1] + raster_after[c[i]-a:c[i]+size-a,d[i]-b:d[i]+size-b,1])/2
+        mean_NIR = raster_before[c[i]-a:c[i]+size-a,d[i]-b:d[i]+size-b,1]/2 + raster_after[c[i]-a:c[i]+size-a,d[i]-b:d[i]+size-b,1]/2
 
-        final_raster = np.dstack((cut_ndvi_before,cut_ndvi_after,mean_NIR))
+        final_raster = np.dstack((cut_ndvi_before,cut_ndvi_after,mean_NIR)).astype(np.float32)
         image_cut_mask = Image.fromarray(cut_mask*255)
-        if ((cut_ndvi_after== 0).sum()) != 0 | ((cut_ndvi_before== 0).sum()) != 0:
+        if (final_raster== 0).sum() != 0:
             continue
         
         if (os.path.isdir(out_path + '/Images')) & (os.path.isdir(out_path + '/Masks')) == False:
             os.mkdir(out_path + '/Images')
             os.mkdir(out_path + '/Masks')
         
+      
+
         tifffile.imsave(out_path+'/Images'+'/image_'+str(i)+'.tiff',final_raster,planarconfig = 'contig')
         image_cut_mask.save(out_path+'/Masks'+'/mask_' + str(i)+'.png')
         
@@ -64,3 +87,5 @@ def make_save_subimages(raster_before_path,raster_after_path,im_path_1,im_path_2
         used_pixels_array_x = used_pixels_sub_array[0] + c[i] - a
         used_pixels_array_y = used_pixels_sub_array[1] + d[i] - b
         mask[used_pixels_array_x,used_pixels_array_y] = 0
+    
+    #change_names()
